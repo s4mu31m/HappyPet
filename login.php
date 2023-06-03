6c72248c4a52a876d33fb5e47650180b3a90c377
Erikgonzalezm <111469422+Erikgonzalezm@users.noreply.github.com>
2023-06-02 22:14:49 -0400

    modigicacion de login y css de login

--- login.php
+++ login.php
@@ -1,10 +1,12 @@
 <?php require "./partials/header.php" ?>
 <body>
-  <h1>Iniciar sesión</h1>
+  
   <div class="container">
+    <h1 >Bienvenido</h1>
+    
     <form action="validar.php" method="POST">
       <div class="form-group ">
-        <label for="username">Nombre de usuario:</label>
+        <label for="username">Correo:</label>
         <input type="text" id="username" name="username" pattern="[a-zA-Z0-9]+" oninput="validateUsername(this)" required>
       </div>
       <div class="form-group ">
@@ -14,7 +16,10 @@
       <div class="form-group ">
         <button type="submit">Iniciar sesión</button>
         <link rel="stylesheet" href="./index.html">
+      
+        <a id ="crearcuenta" href="">Crea una cuenta</a>
+        <a id ="recupera" href="">Recuperar Contraseña</a>
       </div>
     </form>
-  </div>
+  </div> 
 <?php require "./partials/header.php" ?>
