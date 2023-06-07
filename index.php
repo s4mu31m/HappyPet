ee496c20dd50ffa2a5f1d3ec2f180e0f740b070c
S4MU31M <samu.montiel11@gmail.com>
2023-06-06 23:41:37 -0400

    se añade función de calificación de productos
    Se agrega un sistema para mostrar la calificación del producto, esto basado en el parámetro definido en el JSON

--- index.php
+++ index.php
@@ -62,6 +62,19 @@ $productos = json_decode($json);
           echo "<img class='imagen_producto' src='" . $producto->image . "' alt='Imagen del producto'>";
           echo "<p class='descripcion_prodcuto'>" . $producto->description . "</p>";
           echo "<p class='precio_producto'>Precio: " . $producto->price . "</p>";
+
+          // Procesar la calificación
+          $calificacion = round($producto->rating->rate);
+          echo "<div class='estrellas'>";
+          for ($i = 1; $i <= 5; $i++) {
+            // Si el valor de calificación es mayor que o igual a $i, entonces esta estrella debe estar completa
+            if ($calificacion >= $i) {
+              echo "<span class='completa'></span>";
+            } else {
+              echo "<span></span>";
+            }
+          }
+          echo "</div>";
           ?>
         </div>
       <?php endforeach; ?>
