d0af1081bdb000035b9a40a1eca22d5b095c36bc
CatalinaVG <catalinaveliz.g@gmail.com>
2023-07-06 23:51:40 -0600

    funcion eliminar producto carrito

--- public/carrito.php
+++ public/carrito.php
@@ -7,8 +7,15 @@ if (isset($_POST['cart'])) {
     $producto_id = $_POST['product_id'];
     $_SESSION['carrito'][] = $producto_id;
 };
-$productos = json_decode(file_get_contents('api.json'), true);
+// * Crear Conexión con MongoDb
+require '../public/mongo.php';
+$client = connectMongoDB();
 
+// * Buscar en la Base de Datos correspondiente
+$db = $client->happypet;
+$collection = $db->products;
+// * Guardarlos en una Variable.
+$cursor =$collection->find();
 require "../partials/header.php";
 require "../partials/navbar.php";
 
@@ -19,7 +26,7 @@ require "../partials/navbar.php";
     <div>
         <main class="productos">
             <?php foreach ($_SESSION['carrito'] as $producto_id) {
-                    foreach ($productos as $producto) {
+                    foreach ($cursor as $producto) {
                         if ($producto['id'] == $producto_id): ?>
                             <div class="product">
                             <?php
@@ -41,14 +48,16 @@ require "../partials/navbar.php";
                                     <path d='M5 12l14 0' />
                                 </svg>
                             </div>
-
-
-                            <span class='btn-eliminar'></span>
-                            <svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-trash-filled' width='44' height='44' viewBox='0 0 24 24' stroke-width='1.5' stroke='#000000' fill='none' stroke-linecap='round' stroke-linejoin='round'>
-                                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-                                <path d='M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16z' stroke-width='0' fill='currentColor' />
-                                <path d='M14 2a2 2 0 0 1 2 2a1 1 0 0 1 -1.993 .117l-.007 -.117h-4l-.007 .117a1 1 0 0 1 -1.993 -.117a2 2 0 0 1 1.85 -1.995l.15 -.005h4z' stroke-width='0' fill='currentColor' />
-                            </svg>
+                            <form method='post'>
+                                <input type='hidden' name='remove_product_id' value='" . $producto['id'] . "'>
+                                <button type='submit' name='remove' style='background: none; border: none; padding: 0; cursor: pointer;'>
+                                    <svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-trash-filled' width='44' height='44' viewBox='0 0 24 24' stroke-width='1.5' stroke='#000000' fill='none' stroke-linecap='round' stroke-linejoin='round'>
+                                        <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                                        <path d='M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16z' stroke-width='0' fill='currentColor' />
+                                        <path d='M14 2a2 2 0 0 1 2 2a1 1 0 0 1 -1.993 .117l-.007 -.117h-4l-.007 .117a1 1 0 0 1 -1.993 -.117a2 2 0 0 1 1.85 -1.995l.15 -.005h4z' stroke-width='0' fill='currentColor' />
+                                    </svg>
+                                </button>
+                            </form>
                             ";
                             break;
                             ?> </div> <?php
